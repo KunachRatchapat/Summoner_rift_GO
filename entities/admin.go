731619beff5664296ecf0363db84e7d0b82fc2b6
Tehdev <kunatratchapat555@gmail.com>
2025-06-26 02:54:32 +0700

    :heavy_plus_sign: entitie : admin , inventory

--- entities/admin.go
+++ entities/admin.go
@@ -1,1 +1,13 @@
 package entities
+
+import "time"
+
+type (
+	Admin struct {
+		ID       uint64 	`gorm:"primaryKey;autoIncrement;"`
+		Name     string	 	`gorm:"type:varchar(128);"`
+		Email    string 	`gorm:"type:varchar(128);"`
+		Avatar   string 	`gorm:"type:varchar(256);"`
+		CreateAt time.Time 	`gorm:"not noull;autoCreateTime;"`
+	}
+)
--- entities/inventory.go
+++ entities/inventory.go
@@ -1,1 +1,13 @@
 package entities
+
+import "time"
+
+type (
+	Inventory struct {
+		ID        uint64    	`gorm:"primaryKey;autoIncrement;"`
+		Player_ID string   	 	`gorm:"type:varchar(64);not null;"`
+		Card_ID   string   	 	`gorm:"type:varchar(128);not null;"`
+		DeletedAt bool      	`gorm:"not null;default:false"`
+		CreateAt  time.Time 	`gorm:"type:not null; autoCreateTime;"`
+	}
+)
